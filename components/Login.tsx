b21fefa9de9e0ab70bbf592ae390b111049576ee
MeHere120 <mihirpatel1705@gmail.com>
2024-01-13 19:22:07 +0000

    m

--- components/Login.tsx
+++ components/Login.tsx
@@ -70,7 +70,10 @@ export default function Login() {
           </Button>
         </div>
         <div className="mt-6">
-          <Button className="w-full rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 dark:hover:bg-blue-800" onClick={() => return <AccountCreation /> }>
+          <Button
+            className="w-full rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 dark:hover:bg-blue-800"
+            onClick={() => <AccountCreation />}
+          >
             Create Account
           </Button>
         </div>
@@ -82,5 +85,6 @@ export default function Login() {
           </Alert>
         </div>
       </div>
-    </div>  );
+    </div>
+  );
 }

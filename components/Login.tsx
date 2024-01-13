e8dc562196647931d2fd3de0190c680b0b2e7a63
MeHere120 <mihirpatel1705@gmail.com>
2024-01-13 19:34:31 +0000

    m

--- components/Login.tsx
+++ components/Login.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Eye } from "lucide-react";
 import { EyeOff } from "lucide-react";
+import { AccountCreation } from "@/components/AccountCreation";
 import { useState } from "react";
 
 export default function Login() {
@@ -69,10 +70,7 @@ export default function Login() {
           </Button>
         </div>
         <div className="mt-6">
-          <Button
-            className="w-full rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 dark:hover:bg-blue-800"
-            onClick={() => alert("Create Account clicked!")}
-          >
+          <Button className="w-full rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 dark:hover:bg-blue-800">
             Create Account
           </Button>
         </div>

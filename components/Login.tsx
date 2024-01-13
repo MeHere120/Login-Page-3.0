1ed586a4e0ccae364458aeb2b4cbc0ac5bfb2fee
MeHere120 <mihirpatel1705@gmail.com>
2024-01-13 18:49:45 +0000

    made button work

--- components/Login.tsx
+++ components/Login.tsx
@@ -68,11 +68,12 @@ export default function Login() {
             Login
           </Button>
         </div>
-
         <div className="mt-6">
           <Button
             className="w-full rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 dark:hover:bg-blue-800"
-            onClick={() => setOpenComponent("AccountCreation")}
+            onClick={() => {
+              /* TODO: Implement navigation to create account component */
+            }}
           >
             Create Account
           </Button>

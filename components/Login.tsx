3cc8ec7e84c97d9451e1587986b86bcd9d662aa1
MeHere120 <mihirpatel1705@gmail.com>
2024-01-13 19:09:33 +0000

    m

--- components/Login.tsx
+++ components/Login.tsx
@@ -69,12 +69,7 @@ export default function Login() {
           </Button>
         </div>
         <div className="mt-6">
-          <Button
-            className="w-full rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 dark:hover:bg-blue-800"
-            onClick={() => {
-              navigate("/account-creation");
-            }}
-          >
+          <Button className="w-full rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 dark:hover:bg-blue-800">
             Create Account
           </Button>
         </div>

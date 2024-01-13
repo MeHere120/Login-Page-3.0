c28907c72c1c2a3a7b11285805a0aa5500188a85
MeHere120 <mihirpatel1705@gmail.com>
2024-01-13 19:07:12 +0000

    login

--- components/Login.tsx
+++ components/Login.tsx
@@ -72,8 +72,7 @@ export default function Login() {
           <Button
             className="w-full rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 dark:hover:bg-blue-800"
             onClick={() => {
-              /* Navigation to create account component implemented */
-              // TODO: Implement component opening logic here
+              navigate("/account-creation");
             }}
           >
             Create Account
